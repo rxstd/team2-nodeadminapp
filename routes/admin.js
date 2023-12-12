341732e27a9ddc86d0a5d7c5f4d3162f004db958
rxstd <xcoding@protonmail.com>
2023-12-12 16:58:06 +0900

    update main

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,125 @@
 var express = require("express");
 var router = express.Router();
+var dotenv = require("dotenv");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+dotenv.config();
+
+const adminMIddleware = require("../middleware/admin.middleware");
+const adminStore = require("../store/admin.store");
+
+const title = process.env.PROJECT_TITLE;
+
+router.get("/list", adminMIddleware, function (req, res, next) {
+  res.render("admin/list", {
+    title: title,
+    title_ko: "관리자 계정 목록",
+    path: "admin",
+    adminList: adminStore.getAdmins(),
+  });
+});
+
+router.get("/create", adminMIddleware, function (req, res, next) {
+  res.render("admin/create", {
+    title: title,
+    title_ko: "관리자 계정 생성",
+    path: "admin",
+  });
+});
+
+router.post("/create", adminMIddleware, function (req, res, next) {
+  //TODO : admin 계정 저장 프로세스
+  // admin_name: test3
+  // dept_name: test3
+  // used_yn_code: N
+  // pwd1: 1234
+  // pwd2: 1234
+  // email: extoredkom@gmail.com
+  // telephone: 01012341234
+
+  if (req.body.pwd1 !== req.body.pwd2) {
+    res.send(
+      `<script>alert('변경할 비밀번호 입력과 비밀번호 재입력이 일치하지 않습니다.');history.back();</script>`
+    );
+  }
+
+  let admin = {
+    admin_member_id: adminStore.adminDB.length + 1,
+    company_code: 1,
+    admin_id: req.body.admin_id,
+    admin_password: req.body.pwd1,
+    admin_name: req.body.admin_name,
+    email: req.body.email,
+    telephone: req.body.telephone,
+    dept_name: req.body.dept_name,
+    used_yn_code: req.body.used_yn_code,
+    reg_user_id: 1,
+    edit_user_id: 1,
+    edit_date: new Date(Date.now()).toLocaleString(),
+    reg_date: new Date(Date.now()).toLocaleString(),
+  };
+
+  adminStore.adminDB.push(admin);
+
+  res.redirect("/admin/list");
+});
+
+router.get("/modify", adminMIddleware, function (req, res, next) {
+  let adminInfo = adminStore.getAdminById(req.query.admin_id);
+
+  console.log(adminInfo);
+
+  res.render("admin/modify", {
+    title: title,
+    title_ko: "관리자 계정 정보 수정",
+    path: "admin",
+    adminInfo: adminInfo,
+  });
+});
+
+router.post("/modify", adminMIddleware, function (req, res, next) {
+  //TODO : admin 계정 수정 프로세스
+  //admin_member_id: 1
+  //admin_name: 관리자1
+  //dept_name: 개발팀
+  //pwd1: 123
+  //pwd2: 123
+  //used_yn_code: N
+  //email: admin1@gmail.com
+  //telephone: 010-1234-5678
+
+  if (req.body.pwd1 !== req.body.pwd2) {
+    res.send(
+      `<script>alert('변경할 비밀번호 입력과 비밀번호 재입력이 일치하지 않습니다.');history.back();</script>`
+    );
+  }
+
+  let adminIdx = adminStore.adminDB.findIndex(
+    (admin) => admin.admin_member_id === parseInt(req.body.admin_member_id)
+  );
+
+  adminStore.adminDB[adminIdx].admin_name = req.body.admin_name;
+  adminStore.adminDB[adminIdx].dept_name = req.body.dept_name;
+  adminStore.adminDB[adminIdx].used_yn_code = req.body.used_yn_code;
+  adminStore.adminDB[adminIdx].email = req.body.email;
+  adminStore.adminDB[adminIdx].telephone = req.body.telephone;
+
+  if (req.body.pwd1 !== "") {
+    adminStore.adminDB[adminIdx].admin_password = req.body.pwd1;
+  }
+
+  res.redirect("/admin/list");
+});
+
+router.get("/delete", adminMIddleware, function (req, res, next) {
+  //TODO : admin 계정 삭제 프로세스
+
+  let adminIdx = adminStore.adminDB.findIndex(
+    (admin) => admin.admin_member_id === parseInt(req.query.admin_id)
+  );
+
+  adminStore.adminDB.splice(adminIdx, 1);
+
+  res.redirect("/admin/list");
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,46 @@
 var express = require("express");
 var router = express.Router();
+var dotenv = require("dotenv");
 
-var user = require("../models/user.model");
+dotenv.config();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "갱기도", user: false });
-});
+const adminMIddleware = require("../middleware/admin.middleware");
+const adminStore = require("../store/admin.store");
+
+const title = process.env.PROJECT_TITLE;
+
+// -routes\index.js - 공통기능 제공(관리자 사이트 로그인/메인-대시보드)
+// ㄴrouter.get('/login'),views\login.ejs - 로그인 웹페이지 요청 및 응답
+// ㄴrouter.post('/login') -로그인 처리 요청 및 응답,로그인 완료 후 메인 페이지 이동처리
+// ㄴrouter.get('/'),views\index.ejs - 로그인 후 접속할 메인 페이지 요청 및 응답
 
 router.get("/login", function (req, res, next) {
-  res.render("login", { title: "갱기도", error: false });
+  res.render("login", { title: title, title_ko: "로그인", path: "login" });
 });
 
 router.post("/login", function (req, res, next) {
-  const userName = req.body.username;
-  const userPassword = req.body.pass;
+  //TODO : 관리자 로그인 프로세스 - 완료
+  let admin_username = req.body.admin_username;
+  let admin_password = req.body.admin_password;
 
-  const userAuth = user.authorizeUser(userName, userPassword); // user model에서 authorizeUser 함수로 유저 검증 (DB의 역할을 배열로 대체했음)
+  console.log(admin_username, admin_password);
 
-  if (userAuth.status == "-1") {
-    // userName이 존재하지 않을 경우
-    res.render("login", { title: "갱기도", error: userAuth.error });
+  let admin = adminStore.authAdmin(admin_username, admin_password);
+
+  if (admin) {
+    req.session.admin_id = admin.id;
+    res.redirect("/");
+  } else {
+    res.send(
+      `<script>alert('로그인에 실패했습니다.');history.back();</script>`
+    );
   }
 
-  if (userAuth.status == "-2") {
-    // userPassword가 일치하지 않을 경우
-    res.render("login", { title: "갱기도", error: userAuth.error });
-  }
+  res.redirect("/");
+});
 
-  if (userAuth.status == "1") {
-    // 유저 검증에 성공했을 경우
-    console.log(userAuth.data);
-    res.render("index", { title: "갱기도", user: userAuth.data });
-  }
+router.get("/", adminMIddleware, function (req, res, next) {
+  res.render("index", { title: title, title_ko: "대시보드", path: "index" });
 });
 
 module.exports = router;
